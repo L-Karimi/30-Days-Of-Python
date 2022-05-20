99c64352277de0c04496b8755c74baa4e9f17dd1
L-Karimi <luciahkei101@gmail.com>
2022-05-20 21:24:02 +0300

    added day 12

---
+++ day_12/modues.py
@@ -0,0 +1,18 @@
+import mymodule
+from math import *
+
+print(mymodule.generate_full_name('Luciah', 'Wangeci')) # Asabeneh Yetayeh
+def generate_full_name(firstname, lastname):
+    return firstname + ' ' + lastname
+from mymodule import generate_full_name, sum_two_nums, person, gravity
+print(generate_full_name('Asabneh','Yetayeh'))
+print(sum_two_nums(1,9))
+mass = 100;
+weight = mass * gravity
+print(weight)
+print(person['firstname'])
+print(pi)                  # 3.141592653589793, pi constant
+print(sqrt(2))             # 1.4142135623730951, square root
+print(pow(2, 3))           # 8.0, exponential
+print(floor(9.81))         # 9, rounding to the lowest
+print(ceil(9.81))          # 10, rounding to the highest
